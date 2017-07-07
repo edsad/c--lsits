359fb48e0682e722381e8c7753c8e2573d5ac091
Ed Sadler <ed_lorie@comcast.net>
2017-07-07 15:08:02 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            List<string> planetListB = new List<string>(){"Uranius", "Neptune"};
+            planetList.AddRange(planetListB);
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+            planetList.Add("Pluto");
+            Console.WriteLine(planetList);
+
+
+            foreach(string x in planetList){
+                Console.WriteLine(x);
+            }   
+        }
+    }
+}
