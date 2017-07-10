26d4df6c994be466d24806059db36b01f9a5ce2c
Ed Sadler <ed_lorie@comcast.net>
2017-07-10 14:40:02 -0500

    corrected spelling

--- Program.cs
+++ Program.cs
@@ -10,12 +10,16 @@ namespace lists
             List<string> planetList = new List<string>(){"Mercury", "Mars"};
             planetList.Insert(1, "Venus");
             planetList.Insert(2, "Earth");
-            List<string> planetListB = new List<string>(){"Uranius", "Neptune"};
+            List<string> planetListB = new List<string>(){"Uranus", "Neptune"};
             planetList.AddRange(planetListB);
             planetList.Add("Jupiter");
             planetList.Add("Saturn");
             planetList.Add("Pluto");
             Console.WriteLine(planetList);
+
+            string[] output = planetList.GetRange(2,2).ToArray();
+
+            planetList.RemoveAt(8);
 
 
             foreach(string x in planetList){
